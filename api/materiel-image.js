084d7f104942bfe01e052e3405ea6706d210f55a
sebyastien-netizen <sebyastien@gmail.com>
2026-07-30 09:12:13 +0200

    Create materiel-image.js

---
+++ api/materiel-image.js
@@ -0,0 +1,8 @@
+POST /api/materiel-image
+{
+  "ids": [
+    {"id": "hawthorne", "nom": "Passoire Hawthorne", "requete": "Hawthorne cocktail strainer bartending tool"},
+    {"id": "syphon", "nom": "Syphon / Soda siphon", "requete": "iSi soda siphon cocktail carbonation bartending"},
+    {"id": "centrifugeuse", "nom": "Centrifugeuse de laboratoire", "requete": "laboratory benchtop centrifuge tabletop juice clarification"}
+  ]
+}
