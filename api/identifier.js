21ae29eaac607ccd79bbbecdc7add734dd446f49
sebyastien-netizen <sebyastien@gmail.com>
2026-07-31 16:06:18 +0200

    Update identifier.js

--- api/identifier.js
+++ api/identifier.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
           },
           {
             role: 'user',
-            content: 'Identifie cet alcool : "' + nom + '". Retourne ce JSON exactement : {"identifie":true,"trop_vague":false,"categorie_id":"gin","degre":40,"description":"description courte","origine":"origine courte","anecdote":"anecdote courte"}. categorie_id doit etre parmi : liqueurs, gin, vodka, whisky, mezcal-tequila, rhum, eaux-de-vie, bulles, bitters, vermouth, triples-secs, sirops. Si trop vague : identifie false, trop_vague true. Si inconnu : identifie false.'
+            content: 'Identifie cet alcool : "' + nom + '". Retourne ce JSON exactement : {"identifie":true,"trop_vague":false,"categorie_id":"gin","sous_type_alcool":"london-dry","tourbe":false,"degre":40,"description":"description courte","origine":"origine courte","anecdote":"anecdote courte"}. categorie_id doit etre parmi : liqueurs, gin, vodka, whisky, mezcal-tequila, rhum, eaux-de-vie, bulles, bitters, vermouth, triples-secs, sirops. sous_type_alcool depend de categorie_id, vocabulaire controle uniquement (mets null si aucun ne correspond clairement, jamais un mot invente) : pour gin -> london-dry, old-tom, plymouth, contemporain, aromatise, genievre ; pour vodka -> neutre, aromatisee ; pour rhum -> blanc, ambre, brun, agricole, epice, overproof, cachaca ; pour whisky -> scotch-blended, scotch-single-malt, bourbon, rye, irlandais, japonais ; pour mezcal-tequila -> blanco, reposado, anejo, mezcal ; pour eaux-de-vie -> cognac, armagnac, brandy, calvados, pisco ; pour vermouth -> sec, rouge, bianco, sherry, porto ; pour triples-secs -> orange, bleu, marasquin ; pour liqueurs -> herbes-amers, fruits-creme, cafe-creme ; pour bitters -> null toujours ; pour bulles -> champagne, prosecco ; pour sirops -> null toujours. tourbe est un boolean independant, vrai uniquement si explicitement fume/tourbe (surtout whisky Islay), sinon false. Si trop vague : identifie false, trop_vague true. Si inconnu : identifie false.'
           }
         ]
       })
@@ -52,4 +52,3 @@ export default async function handler(req, res) {
     return res.status(200).json({ identifie: false, trop_vague: false, error: e.message });
   }
 }
- 
